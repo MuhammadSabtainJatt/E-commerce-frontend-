0edcf9c8a730985c6f4ef34349e3f63e1cf5ae57
Muhammad Sabtain <subtainjutt26@gmail.com>
2024-03-25 23:01:13 +0500

    Header Develop

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 import './App.scss';
-import Page from '../src/Frountend/Pages/Contact'
+import Page from '../src/components/Header'
 // import Page from './Auth/Login/index'
 function App() {
   return (
