02bc63e5baac2b9ea7115ea5129edbdefaaa2bff
Muhammad Sabtain <subtainjutt26@gmail.com>
2024-03-26 13:40:53 +0500

    Routes are define

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 import './App.scss';
-import Page from '../src/components/Header'
-// import Page from './Auth/Login/index'
+import Routes from './Routes'
 function App() {
   return (
    <>
-   <Page />
+   <Routes />
    </>
   );
 }
