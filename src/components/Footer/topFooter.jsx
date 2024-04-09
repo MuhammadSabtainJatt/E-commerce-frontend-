10a64719850f035274f655b49c8250a3952cd352
Muhammad Sabtain <subtainjutt26@gmail.com>
2024-04-09 23:11:09 +0500

    .

--- src/components/Footer/topFooter.jsx
+++ src/components/Footer/topFooter.jsx
@@ -37,3 +37,120 @@ export default function footer() {
 
 
 
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function footer() {
+  return (
+
+    <>
+      <div className="container " style={{ minHeight: "80vh", backgroundColor: "rgb(1, 7, 29)" }}>
+        <div className="row">
+          <div className="col">
+            <h4 className='text-light text-center'>
+              Subscribe To Get Offers In Your Inbox
+            </h4>
+            <p className='text-light'>
+              Lorem ipsum dolor sit amet, adipiscing elit sed do eiusmod condimentum
+            </p>
+            <div>
+              <input type="text" className='w-50 d-inline' placeholder='Your Email Address ...' />
+              <button>
+                Subscribe
+              </button>
+            </div>
+            <div>
+              <button>
+                <Link >
+                  ghh
+                </Link>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+
+
+
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function footer() {
+  return (
+
+    <>
+      <div className="container " style={{ minHeight: "80vh", backgroundColor: "rgb(1, 7, 29)" }}>
+        <div className="row">
+          <div className="col">
+            <h4 className='text-light text-center'>
+              Subscribe To Get Offers In Your Inbox
+            </h4>
+            <p className='text-light'>
+              Lorem ipsum dolor sit amet, adipiscing elit sed do eiusmod condimentum
+            </p>
+            <div>
+              <input type="text" className='w-50 d-inline' placeholder='Your Email Address ...' />
+              <button>
+                Subscribe
+              </button>
+            </div>
+            <div>
+              <button>
+                <Link >
+                  ghh
+                </Link>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+
+
+
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function footer() {
+  return (
+
+    <>
+      <div className="container " style={{ minHeight: "80vh", backgroundColor: "rgb(1, 7, 29)" }}>
+        <div className="row">
+          <div className="col">
+            <h4 className='text-light text-center'>
+              Subscribe To Get Offers In Your Inbox
+            </h4>
+            <p className='text-light'>
+              Lorem ipsum dolor sit amet, adipiscing elit sed do eiusmod condimentum
+            </p>
+            <div>
+              <input type="text" className='w-50 d-inline' placeholder='Your Email Address ...' />
+              <button>
+                Subscribe
+              </button>
+            </div>
+            <div>
+              <button>
+                <Link >
+                  ghh
+                </Link>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+
+
+
