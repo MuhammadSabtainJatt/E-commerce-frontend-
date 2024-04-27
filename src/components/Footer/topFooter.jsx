f907d72d07c542e8ab5dcaad6ab2782cae894706
Muhammad Sabtain <subtainjutt26@gmail.com>
2024-04-27 22:15:32 +0500

    Update Code

--- src/components/Footer/topFooter.jsx
+++ src/components/Footer/topFooter.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookF, faInstagram, faTelegram, faTwitter, } from '@fortawesome/free-brands-svg-icons'
 import { Link } from 'react-router-dom'
 
 export default function footer() {
   return (
 
     <>
-      <div className="container " style={{ minHeight: "80vh", backgroundColor: "rgb(1, 7, 29)" }}>
-        <div className="row">
+      <div className="container " style={{ minHeight: "80vh", backgroundColor: "rgb(1, 7, 29)" ,display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
+        <div className="row text-center">
           <div className="col">
             <h4 className='text-light text-center'>
               Subscribe To Get Offers In Your Inbox
@@ -21,24 +23,31 @@ export default function footer() {
               </button>
             </div>
             <div>
-              <button>
-                <Link >
-                  Facebook
+              <button style={{backgroundColor:"transparent",border:"none",margin:"20px"}}>
+                <Link className='text-white'>
+                  <h2>
+                <FontAwesomeIcon icon={faFacebookF} />
+                </h2>
                 </Link>
               </button>
-              <button>
-                <Link >
-                  Twitter
+              <button style={{backgroundColor:"transparent",border:"none",margin:"20px"}}>
+                <Link className='text-white'>
+                  <h2>
+                <FontAwesomeIcon icon={faTwitter} />
+                  </h2>
                 </Link>
               </button>
-              <button>
-                <Link >
-                  Instragram
+              <button style={{backgroundColor:"transparent",border:"none",margin:"20px"}}>
+                <Link className='text-white'>
+                  <h2>
+                <FontAwesomeIcon icon={faInstagram} />
+                </h2>
                 </Link>
               </button>
-              <button>
-                <Link >
-                  Other Websites
+              <button style={{backgroundColor:"transparent",border:"none",margin:"20px"}}>
+                <Link className='text-white'>
+                <h2>
+                <FontAwesomeIcon icon={faTelegram} /></h2>
                 </Link>
               </button>
 
