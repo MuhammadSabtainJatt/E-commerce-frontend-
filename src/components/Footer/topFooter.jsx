9c8483c8213a882a43df24fe659c7a60a4a74a0b
Muhammad Sabtain <subtainjutt26@gmail.com>
2024-05-08 22:17:09 +0500

    Footer Completed Successfully

--- src/components/Footer/topFooter.jsx
+++ src/components/Footer/topFooter.jsx
@@ -7,18 +7,18 @@ export default function footer() {
   return (
 
     <>
-      <div className="container " style={{ minHeight: "80vh", backgroundColor: "rgb(1, 7, 29)" ,display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
+      <div className="container " style={{ minHeight: "60vh", backgroundColor: "rgb(1, 7, 29)" ,display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
         <div className="row text-center">
           <div className="col">
-            <h4 className='text-light text-center'>
+            <h2 className='text-light text-center'>
               Subscribe To Get Offers In Your Inbox
-            </h4>
+            </h2>
             <p className='text-light'>
               Lorem ipsum dolor sit amet, adipiscing elit sed do eiusmod condimentum
             </p>
             <div>
-              <input type="text" className='w-50 d-inline' placeholder='Your Email Address ...' />
-              <button>
+              <input type="text"  style={{height:"50px"}}className='p-2 rounded border-0 w-50 d-inline' placeholder='Your Email Address ...' />
+              <button  style={{height:"50px"}}className='p-2  rounded ms-2 border-0 d-inline'>
                 Subscribe
               </button>
             </div>
