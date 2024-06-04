c4989d24e3b28ab3c568fc7bc518effd2991c232
Muhammad Sabtain <subtainjutt26@gmail.com>
2024-06-04 22:11:02 +0500

    Code Update

--- src/Frountend/Pages/About/about.jsx
+++ src/Frountend/Pages/About/about.jsx
@@ -52,7 +52,7 @@ export default function about() {
             </p>
           </div>
           <div className="col-12 col-md-6 p-0">
-            <img src={image} style={{ width: "100%", height: "90vh" }} alt="" />
+            <img src={image} style={{ width: "100%", height: "100vh" }} alt="" />
           </div>
         </div>
         haekjbhlk
